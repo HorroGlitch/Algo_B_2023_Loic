766612d12ad8f2659b17294eb9838df81f3df669
HorroGlitch <lverot@edenschool.fr>
2023-03-20 16:56:01 +0100

    adding algo

---
+++ js/app.js
@@ -0,0 +1,112 @@
+// Exo 1
+
+// let resultat = "";
+// for (let i = 0; i < 11; i++){
+//    resultat += i;
+// }
+// console.log(resultat);
+
+
+// Exo 2
+// let resultat = "";
+// for (let i = 0; i < 11; i++){
+//    resultat += i+ " ";
+// }
+// console.log(resultat);
+
+// Exo 3
+// let resultat = "";
+// for (let i = 1; i < 101; i++){
+    
+//    resultat += i + " ";
+
+// }
+// console.log(resultat);
+
+//  Exo 4
+// let resultat = " ";
+// for (let i = 0; i < 11; i++){
+//     resultat += i*7 + " ";
+//  }
+//  console.log(resultat);
+
+// Exo 5
+
+// let resultat = " ";
+// for (let i = 0; i < 11; i++){
+
+//     for (let j = 1; j < 11 ; j++)
+//     resultat += j + " ";
+
+//  }
+//  console.log(resultat);
+
+
+// Exo 6
+
+// let resultat = 0;
+// for (let i = 1; i < 11; i++){
+//    resultat += i;
+// }
+// console.log(resultat);
+
+
+// Exo 7
+
+// let resultat = 0;
+
+// for (let i = 1; i < 11; i++){
+//    resultat *= i;
+
+// }
+// console.log(resultat);
+
+// Exo 8
+
+// let resultat = 0;
+// function 
+
+// Exo 9
+
+// let celsiusNum = 30;
+// let fahrenheitNum = 0;
+
+// function fahrenheit (){
+//     (celsiusNum *  9/5) + 32;
+// }
+
+// fahrenheit = (celsiusNum *  9/5) + 32;
+
+// console.log(fahrenheit);
+
+
+// Exo 10
+
+// let tab = [1,5,9,3,7];
+// let sommeTotal = 0;
+
+// function somme (tab){
+//     for (let i = 0; i < tab; i++){
+//         sommeTotal += tab[i]
+//     }
+// }
+// console.log(sommeTotal);
+
+// Exo 11
+
+let numbers = [15,8,9,11,12];
+let somme = 0;
+
+function moyenne (numbers){
+    for (let i = 0; i < numbers.length ; i++){
+        somme += numbers[i];
+    }
+}
+console.log(somme);
+
+// Exo 12 
+
+// function positif(tableau) 
+
+
+
